186def491f0f622d46a08ba633132acfa25fa61e
wuqiguang <wuqiguang@ycmedic.com>
2018-06-07 15:16:57 +0800

    create project

---
+++ src/main/java/com/wqg/boot_db/BootDbApplication.java
@@ -0,0 +1,12 @@
+package com.wqg.boot_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wqg/boot_db/BootDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wqg.boot_db;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootDbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
